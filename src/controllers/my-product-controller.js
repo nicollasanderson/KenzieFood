188d2d866eae104c77228a8bc4993c043ede234f
igorcenzi <igor.cenzi2@gmail.com>
2022-01-25 16:22:25 -0300

    refactor: my-product-controller parameters

--- src/controllers/my-product-controller.js
+++ src/controllers/my-product-controller.js
@@ -3,67 +3,42 @@ import {RequestAPI} from '../request/requeste.js'
 const urlAPI = 'https://kenzie-food-api.herokuapp.com/my/product'
 const token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTksImlhdCI6MTY0MzExNzgyNiwiZXhwIjoxNjQzOTgxODI2LCJzdWIiOiJbb2JqZWN0IFVuZGVmaW5lZF0ifQ.qqcyKNMDkRvOA-K7-L6vSo7HEW-if-jqwV3WH_yOypA'
 
+const config = {
+  headers: {
+    "Content-Type": "application/json",
+   "Authorization": token
+  }
+}
 
 class MyProductController{
     static async getAll(){
-      const config = {
-        method: "get", 
-        headers: {
-          "Content-Type": "application/json",
-         "Authorization": token
-        }
-      }
+      config.method = "get"
       const data = await RequestAPI.getMyProduct(urlAPI, config)
       return data
     }
 
     static async getById(id){
-      const config = {
-        method: "get", 
-        headers: {
-          "Content-Type": "application/json",
-         "Authorization": token
-        }
-      }
+      config.method = "get"
       const data = await RequestAPI.getMyProduct(`${urlAPI}/${id}`, config)
       return data
     }
 
     static async deleteProduct(id){
-      const config = {
-        method: "delete", 
-        headers: {
-          "Content-Type": "application/json",
-         "Authorization": token
-        }
-      }
+      config.method = "delete"
       const data = await RequestAPI.getMyProduct(`${urlAPI}/${id}`, config)
       return data
     }
 
     static async create(product){
-      const config = {
-        method: "post", 
-        headers: {
-          "Content-Type": "application/json",
-         "Authorization": token
-        },
-        body: JSON.stringify(product)
-      }
+      config.method = "post"
+      config.body = JSON.stringify(product)
       const data = await RequestAPI.getMyProduct(urlAPI, config)
       return data
     }
 
     static async patchProduct(id,product){
-      console.log(product)
-      const config = {
-        method: "PATCH", 
-        headers: {
-          "Content-Type": "application/json",
-         "Authorization": token
-        },
-        body: JSON.stringify(product)
-      }
+      config.method = "PATCH"
+      config.body = JSON.stringify(product)
       const data = await RequestAPI.getMyProduct(`${urlAPI}/${id}`, config)
       console.log(config)
       return data
