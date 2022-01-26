a3da3ab57d286f28b15a301c79a42ba81f43b2d7
vinii <vininunesmarcal@gmail.com>
2022-01-26 12:17:29 -0300

    fix: ajeitando icones

--- src/models/product-model.js
+++ src/models/product-model.js
@@ -11,6 +11,7 @@ class VitriniModel{
             const divCompra = document.createElement('div')
             const pPrice = document.createElement('p')
             const button = document.createElement('button')
+            const imgCart = document.createElement("img")
             
             img.src = element.imagem
             img.alt = element.nome
@@ -18,10 +19,12 @@ class VitriniModel{
             h3.innerText = element.nome
             pDesc.innerText = element.descricao
             pPrice.innerText = `R$ ${element.preco}`
+            imgCart.src = "src/imgs/shopping-cart_1f6d2.png"
             
             divCompra.classList.add('comprar')
             img.classList.add('img')
 
+            button.appendChild(imgCart)
             li.appendChild(img)
             li.appendChild(span)
             li.appendChild(h3)
@@ -48,6 +51,7 @@ class VitriniModel{
             const spantTag = document.createElement('span')
             const pPrice = document.createElement('p')
             const buttonRemove = document.createElement('button')
+            const imgRemove = document.createElement('img')
 
             divInfo.classList.add('infoProdutos')
             buttonRemove.classList.add('remover')
@@ -56,7 +60,9 @@ class VitriniModel{
             nameProductCart.innerText = element.nome 
             spantTag.innerText = element.categoria
             pPrice.innerText = `R$ ${element.preco}`
+            imgRemove.src = "src/imgs/lixo.png"
 
+            buttonRemove.appendChild(imgRemove)
             liContainer.appendChild(imgProductCart)
             divInfo.appendChild(nameProductCart)
             divInfo.appendChild(spantTag)
