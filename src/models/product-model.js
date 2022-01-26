396f1a3f1a2cc14d80f4bdb801121e0f59dcf56f
Nicollas Anderson <nicollas_sdias@hotmail.com>
2022-01-26 17:08:32 -0300

    feat: carrinho funcionando

--- src/models/product-model.js
+++ src/models/product-model.js
@@ -20,12 +20,9 @@ class VitriniModel{
             h3.innerText = element.nome
             pDesc.innerText = element.descricao
             pPrice.innerText = `R$ ${element.preco}`
-            imgCart.src = "src/imgs/shopping-cart_1f6d2.png"
             
             divCompra.classList.add('comprar')
-            img.classList.add('img')
 
-            button.appendChild(imgCart)
             li.appendChild(img)
             li.appendChild(span)
             li.appendChild(h3)
@@ -52,7 +49,6 @@ class VitriniModel{
             const spantTag = document.createElement('span')
             const pPrice = document.createElement('p')
             const buttonRemove = document.createElement('button')
-            const imgRemove = document.createElement('img')
 
             divInfo.classList.add('infoProdutos')
             buttonRemove.classList.add('remover')
@@ -61,9 +57,7 @@ class VitriniModel{
             nameProductCart.innerText = element.nome 
             spantTag.innerText = element.categoria
             pPrice.innerText = `R$ ${element.preco}`
-            imgRemove.src = "src/imgs/lixo.png"
-
-            buttonRemove.appendChild(imgRemove)
+            
             liContainer.appendChild(imgProductCart)
             divInfo.appendChild(nameProductCart)
             divInfo.appendChild(spantTag)
